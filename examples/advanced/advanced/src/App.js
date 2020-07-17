6b8a502393b787fd5282f6b232c465a660e2b5ef
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-07-17 10:41:55 +0700

    chore: add onModelProcessed event

--- examples/advanced/advanced/src/App.js
+++ examples/advanced/advanced/src/App.js
@@ -1,18 +1,13 @@
 import conf from 'src/conf';
 import React from 'react';
-import {
-  StatusBar,
-  Text as RNText
-} from 'react-native';
+import {StatusBar, Text as RNText} from 'react-native';
 
 import {StyleProvider, Root, Input} from 'native-base';
-import {createAppContainer} from "react-navigation";
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import {routesMap} from 'src/routes';
 import MainHeader from 'src/baseComponents/MainHeader';
-
-
 
 // Disable text font scaling, it is a mess with native-base
 RNText.defaultProps = RNText.defaultProps || {};
@@ -22,37 +17,37 @@ Input.defaultProps = Input.defaultProps || {};
 Input.defaultProps.allowFontScaling = false;
 Input.defaultProps.fontSize = conf.theme.variables.DefaultFontSize;
 
-
-
 // Main stack navigator, used for authenticated routes only.
 // Login won't use navigation. If we want further non authenticated screens
 // we will need a separate router for it.
-const StackNavigator = createStackNavigator (
-  routesMap,
-  {
-    initialRouteName: "Home",
-    transitionConfig: () => ({
-      transitionSpec: {
-        isInteraction: true,
-        useNativeDriver: true
-      }
-    }),
+const StackNavigator = createStackNavigator(routesMap, {
+  initialRouteName: 'Home',
+  transitionConfig: () => ({
+    transitionSpec: {
+      isInteraction: true,
+      useNativeDriver: true,
+    },
+  }),
 
-    headerMode: 'screen',
+  headerMode: 'screen',
 
-    defaultNavigationOptions: ({ navigation }) => {
-      return {
-        header: props => {
-          return <MainHeader back={true} title={props.scene.descriptor.options.title} navigation={navigation}/>;
-        },
-        gesturesEnabled: false
-      }
-    }
-  }
-)
+  defaultNavigationOptions: ({navigation}) => {
+    return {
+      header: (props) => {
+        return (
+          <MainHeader
+            back={true}
+            title={props.scene.descriptor.options.title}
+            navigation={navigation}
+          />
+        );
+      },
+      gesturesEnabled: false,
+    };
+  },
+});
 
 const NavigatorApp = createAppContainer(StackNavigator);
-
 
 const App = () => {
   return (
@@ -65,9 +60,7 @@ const App = () => {
         <NavigatorApp />
       </Root>
     </StyleProvider>
-  )
-}
-
-
+  );
+};
 
 export default App;
